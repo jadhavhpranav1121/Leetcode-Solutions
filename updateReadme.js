926c9fe118455fa04238af4fa29dcd02a0d5e030
Pranav Jadhav <jadhavhpranav1121@gmail.com>
2022-08-24 10:45:24 +0530

    New Structure

--- updateReadme.js
+++ updateReadme.js
@@ -6,15 +6,16 @@ let content =
   "# LeetCode\n\n### Solved problems\n\n|Number|Title|Problem|Code|\n|---|---|---|---|\n";
 
 const problems = {};
-fs.readdirSync(".").forEach((file) => {
-  if (!fs.statSync("./" + file).isDirectory() && file.endsWith(".cpp")) {
+fs.readdirSync("./C++ Solutions/").forEach((file) => {
+	console.log(file);
+ 	if (!(fs.statSync("./C++ Solutions/" + file).isDirectory()) && file.endsWith(".cpp")) {
     problems[file.split(".")[0]] = {
       title: fs
-        .readFileSync("./" + file, "utf8")
+        .readFileSync("./C++ Solutions/" + file, "utf8")
         .split("\n")[3]
         .split("] ")[1],
       url: fs
-        .readFileSync("./" + file, "utf8")
+        .readFileSync("./C++ Solutions/" + file, "utf8")
         .split("\n")[5]
         .slice(3)
         .replace("/description/", ""),
@@ -24,6 +25,6 @@ fs.readdirSync(".").forEach((file) => {
   }
 });
 for (var key in problems)
-  content += `|${key}|${problems[key].title}|[Problem](${problems[key].url})|[Code](https://github.com/samba9274/LeetCode/blob/master/${key}.${problems[key].filename}.${problems[key].fileExtension})|\n`;
+  content += `|${key}|${problems[key].title}|[Problem](${problems[key].url})|[Code](https://github.com/Jadhavhpranav1121/LeetCode/blob/master/${key}.${problems[key].filename}.${problems[key].fileExtension})|\n`;
 
 fs.writeFile("README.md", content, (err) => err);
